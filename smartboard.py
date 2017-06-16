eb7ba065a09c101cf46f54e4d73e6b69109a2260
monkrobot <sdenisov93@mail.ru>
2017-06-16 18:01:49 +0300

    Try to combine menu and grid - fail Try to draw by mouse click - drawing_points.py - begining

--- smartboard.py
+++ smartboard.py
@@ -1,8 +1,52 @@
 import sys
-from PyQt5.QtWidgets import (QMainWindow, QWidget, QToolTip, QPushButton, QApplication, QTextEdit, QLineEdit,
-                             QGridLayout, QMessageBox, QLabel)
-from PyQt5.QtGui import (QIcon, QFont)
+from PyQt5.QtWidgets import (QMainWindow, QAction, qApp, QWidget, QToolTip, QPushButton, QApplication, QTextEdit, QLineEdit,
+                             QGridLayout, QMessageBox, QLabel, QFrame, QColorDialog, QFileDialog)
+from PyQt5.QtGui import (QIcon, QFont, QColor)
 from PyQt5.QtCore import QCoreApplication
+
+# Menu
+class Menu(QMainWindow):
+    # file
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        print('MenuFile')
+        # exit
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        # save file
+        saveAction = QAction(QIcon('save.png'), '&Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Save as...')
+        exitAction.triggered.connect(qApp.quit)
+
+        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showFiles)
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(saveAction)
+
+    def showFiles(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
+
+        f = open(fname, 'r')
+
+        with f:
+            data = f.read()
+            self.textEdit.setText(data)
 
 class Board(QWidget):
     def __init__(self):
@@ -16,6 +60,8 @@ class Board(QWidget):
     def initUI(self):
         QToolTip.setFont(QFont('SansSerif', 10))
 
+        self.c = Menu()
+
         #Typing of text
         textEdit = QTextEdit()
         #self.setCentralWidget(textEdit)
@@ -27,19 +73,30 @@ class Board(QWidget):
         #popup help-text
         self.setToolTip('This is a <b>QWidget</b> widget') #Popup help text
 
-        # Delete button
-        delbtn = QPushButton('Delete', self) #Button widget
-        delbtn.setToolTip('This is a Delete') # Popup help text
-        delbtn.clicked.connect(self.printTest)
-        delbtn.resize(delbtn.sizeHint())
+        # Delete button, buttons will be work without 'self'
+        self.delbtn = QPushButton('Delete', self) #Button widget
+        self.delbtn.setToolTip('This is a Delete') # Popup help text
+        self.delbtn.clicked.connect(self.printTest)
+        self.delbtn.resize(self.delbtn.sizeHint())
         #delbtn.move(200, 50)
 
         #Quit button
-        quitbtn = QPushButton('Quit', self)
-        quitbtn.clicked.connect(QCoreApplication.instance().quit)
-        quitbtn.setToolTip('This is a Quit')
-        quitbtn.resize(quitbtn.sizeHint())
+        self.quitbtn = QPushButton('Quit', self)
+        self.quitbtn.clicked.connect(QCoreApplication.instance().quit)
+        self.quitbtn.setToolTip('This is a Quit')
+        self.quitbtn.resize(self.quitbtn.sizeHint())
         #quitbtn.move(200, 100)
+
+        #Color Dialog. If this button will be without 'self', then col will not change color
+        col = QColor(0, 0, 0)
+        self.colbtn = QPushButton('Dialog', self)
+        #colbtn.move(20, 20)
+
+        self.colbtn.clicked.connect(self.showColorDialog)
+
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }" % col.name())
+        #frm.setGeometry(130, 22, 100, 100)
 
         titleEdit = QLineEdit()
         #titleEdit.move(100, 50)
@@ -51,10 +108,13 @@ class Board(QWidget):
         grid.addWidget(greeting_text, 1, 0, 1, 1)
         grid.addWidget(titleEdit, 2, 1)
 
-        grid.addWidget(delbtn, 3, 0)
-        grid.addWidget(quitbtn, 3, 1)
+        grid.addWidget(self.delbtn, 3, 0)
+        grid.addWidget(self.quitbtn, 3, 1)
 
-        grid.addWidget(textEdit, 4, 1)
+        grid.addWidget(self.colbtn, 4, 0)
+        grid.addWidget(self.frm, 4, 1)
+
+        grid.addWidget(textEdit, 6, 1)
 
         self.setLayout(grid)
 
@@ -73,6 +133,14 @@ class Board(QWidget):
             event.accept()
         else:
             event.ignore()
+
+    #Color Dialog
+    def showColorDialog(self):
+        col = QColorDialog.getColor()
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                                   % col.name())
+
 
 if __name__ == '__main__':
 
--- test.py
+++ test.py
@@ -1,7 +1,35 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QWidget, QLCDNumber, QSlider,
-    QVBoxLayout, QApplication)
+from PyQt5.QtWidgets import QPushButton, QWidget, QApplication
+from PyQt5.QtCore import Qt, QMimeData
+from PyQt5.QtGui import QDrag
+
+
+class Button(QPushButton):
+
+    def __init__(self, title, parent):
+        super().__init__(title, parent)
+
+
+    def mouseMoveEvent(self, e):
+
+        if e.buttons() != Qt.RightButton:
+            return
+
+        mimeData = QMimeData()
+
+        drag = QDrag(self)
+        drag.setMimeData(mimeData)
+        drag.setHotSpot(e.pos() - self.rect().topLeft())
+
+        dropAction = drag.exec_(Qt.MoveAction)
+
+
+    def mousePressEvent(self, e):
+
+        QPushButton.mousePressEvent(self, e)
+
+        if e.button() == Qt.LeftButton:
+            print('press')
 
 
 class Example(QWidget):
@@ -14,23 +42,32 @@ class Example(QWidget):
 
     def initUI(self):
 
-        lcd = QLCDNumber(self)
-        sld = QSlider(Qt.Horizontal, self)
+        self.setAcceptDrops(True)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(lcd)
-        vbox.addWidget(sld)
+        self.button = Button('Button', self)
+        self.button.move(100, 65)
 
-        self.setLayout(vbox)
-        sld.valueChanged.connect(lcd.display)
+        self.setWindowTitle('Click or Move')
+        self.setGeometry(300, 300, 280, 150)
 
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Signal & slot')
-        self.show()
+
+    def dragEnterEvent(self, e):
+
+        e.accept()
+
+
+    def dropEvent(self, e):
+
+        position = e.pos()
+        self.button.move(position)
+
+        e.setDropAction(Qt.MoveAction)
+        e.accept()
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = Example()
-    sys.exit(app.exec_())
+    ex.show()
+    app.exec_()
