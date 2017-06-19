7fe7f1d10961d23326151e269003341e7892aa79
monkrobot <sdenisov93@mail.ru>
2017-06-19 14:53:56 +0300

    Test.py - drawing_points.py - determined position of mouse click, but can't use this position for paintEvent.

--- drawing_points.py
+++ drawing_points.py
@@ -1,26 +1,31 @@
-import sys, random
-from PyQt5.QtWidgets import QWidget, QApplication
+import sys, random, re
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 
 
 class Example(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.mouse_pos = [0, 0]
         self.initUI()
 
-
+    #mouse_pos = [0, 0]
     def initUI(self):
 
         self.setGeometry(300, 300, 280, 170)
         self.setWindowTitle('Points')
         self.show()
 
+        greeting_text = QLabel('Welcome', self)
+        greeting_text.move(80, 50)
+
 
     def paintEvent(self, e):
-
+        print('paint')
+        #mouse_pos = self.mousePressEvent()
+        #print('paintEvent: ', mouse_pos)
         qp = QPainter()
         qp.begin(self)
         self.drawPoints(qp)
@@ -28,12 +33,27 @@ class Example(QWidget):
 
 
     def drawPoints(self, qp):
-
+        print('drawPoints')
         qp.setPen(Qt.red)
         size = self.size()*15
         x = 140
         y = 80
         qp.drawPoint(x, y)
+
+    # mouse click position
+    def mousePressEvent(self, e):
+        QWidget.mousePressEvent(self, e)
+
+        if e.button() == Qt.LeftButton:
+
+            slice_e_pos = re.findall("[-0-9]+", str(e.pos()))
+            mouse_pos = [int(slice_e_pos[1]), int(slice_e_pos[2])]
+            print('mouse_pos: ', mouse_pos)
+            print('')
+
+            #return mouse_pos
+            #self.paintEvent(e, mouse_pos)
+    #print(mouse_pos)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,73 +1,63 @@
-import sys
-from PyQt5.QtWidgets import QPushButton, QWidget, QApplication
-from PyQt5.QtCore import Qt, QMimeData
-from PyQt5.QtGui import QDrag
-
-
-class Button(QPushButton):
-
-    def __init__(self, title, parent):
-        super().__init__(title, parent)
-
-
-    def mouseMoveEvent(self, e):
-
-        if e.buttons() != Qt.RightButton:
-            return
-
-        mimeData = QMimeData()
-
-        drag = QDrag(self)
-        drag.setMimeData(mimeData)
-        drag.setHotSpot(e.pos() - self.rect().topLeft())
-
-        dropAction = drag.exec_(Qt.MoveAction)
-
-
-    def mousePressEvent(self, e):
-
-        QPushButton.mousePressEvent(self, e)
-
-        if e.button() == Qt.LeftButton:
-            print('press')
+import sys, random, re
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtCore import Qt, QPoint
 
 
 class Example(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.mouse_pos = [0, 0]
         self.initUI()
 
-
+    #mouse_pos = [0, 0]
     def initUI(self):
 
-        self.setAcceptDrops(True)
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Points')
+        self.show()
 
-        self.button = Button('Button', self)
-        self.button.move(100, 65)
-
-        self.setWindowTitle('Click or Move')
-        self.setGeometry(300, 300, 280, 150)
-
-
-    def dragEnterEvent(self, e):
-
-        e.accept()
+        greeting_text = QLabel('Welcome', self)
+        greeting_text.move(80, 50)
 
 
-    def dropEvent(self, e):
+    def paintEvent(self, e):
+        print('paint')
+        #mouse_pos = self.mousePressEvent()
+        #print('paintEvent: ', mouse_pos)
+        qp = QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
 
-        position = e.pos()
-        self.button.move(position)
 
-        e.setDropAction(Qt.MoveAction)
-        e.accept()
+    def drawPoints(self, qp):
+        print('drawPoints')
+        qp.setPen(Qt.red)
+        size = self.size()*15
+        x = 140
+        y = 80
+        qp.drawPoint(x, y)
+
+    # mouse click position
+    def mousePressEvent(self, e):
+        QWidget.mousePressEvent(self, e)
+
+        if e.button() == Qt.LeftButton:
+
+            slice_e_pos = re.findall("[-0-9]+", str(e.pos()))
+            mouse_pos = [int(slice_e_pos[1]), int(slice_e_pos[2])]
+            print('mouse_pos: ', mouse_pos)
+            print('')
+
+            #return mouse_pos
+            #self.paintEvent(e, mouse_pos)
+    #print(mouse_pos)
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = Example()
-    ex.show()
-    app.exec_()
+    sys.exit(app.exec_())
